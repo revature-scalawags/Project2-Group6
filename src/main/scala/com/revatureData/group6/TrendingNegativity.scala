f85116e081b97791b68bee6105f0103790767650
My Lord <page.c.tyler@gmail.com>
2021-01-19 12:29:14 -0500

    resolved all issues in the negative words trending app.

--- src/main/scala/com/revatureData/group6/TrendingNegativity.scala
+++ src/main/scala/com/revatureData/group6/TrendingNegativity.scala
@@ -1,28 +1,15 @@
 package com.revatureData.group6
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{col, explode, split, udf}
+import org.apache.spark.sql.functions.{broadcast, col, explode, split}
+import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
 
-import scala.io.Source
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 object TrendingNegativity {
 
-  def loadDictionary: Map[String, Int] = {
-    var badWords: Map[String, Int] = Map()
-    val lines = Source.fromFile("data/negative-words.csv")
-
-    for (line <- lines.getLines()) {
-      val fields = line.split(',').map(_.trim)
-      if (fields.length > 1) {
-        badWords += (fields(1) -> 1)
-      }
-    }
-    lines.close()
-
-    badWords
-  }
+  case class BadWords(id: Int, word: String)
 
   def main(args: Array[String]): Unit = {
     val streamer = TweetStreamRunner()
@@ -30,38 +17,48 @@ object TrendingNegativity {
     Future {
       streamer.streamToDirectory()
     }
+
     val spark = SparkSession.builder()
       .appName("TweetsTrending")
       .master("local[*]")
       .getOrCreate()
-    import spark.implicits._
     spark.sparkContext.setLogLevel("WARN")
-    val dictionary = spark.sparkContext.broadcast(loadDictionary)
+
+    val badWordSchema = new StructType()
+      .add("id", IntegerType, nullable = true)
+      .add("word", StringType, nullable = true)
+
+    import spark.implicits._
+    val badWordDictionary = spark.read
+      .option("sep", ",")
+      .schema(badWordSchema)
+      .csv("data/negative-words.csv")
+      .as[BadWords]
 
     val staticDF = spark.read.json("tweetstream.tmp")
     val streamDF = spark.readStream.schema(staticDF.schema).json("data/twitterstream")
     streamDF.printSchema()
 
-    val words = streamDF.select("data.text")
-//      .as("words_array"))
-//      .drop("data.text")
-//      .withColumn("words", explode($"words_array"))
+    val wordsDF = streamDF.select(split(col("data.text"), "\\s+")
+      .as("words_array"))
+      .drop("data.text")
+      .withColumn("words", explode($"words_array"))
 
-    val lookUpWord : String => Int = (tweet: String) => tweet.split(" ").map(word => dictionary.value(word)).sum
-    val lookUpWordUDF = udf(lookUpWord)
+    val badWordJoin = wordsDF.join(
+      broadcast(badWordDictionary),
+      wordsDF("words") <=> badWordDictionary("word")
+    )
+    badWordJoin.explain()
 
-    val badWordCount = lookUpWordUDF(col("data.text"))
-    println(badWordCount)
-    val withCount = streamDF.withColumn("neg_word_count", badWordCount)
-      val query = withCount.groupBy("neg_word_count")
+    val badWordCount = badWordJoin
+      .groupBy("word")
       .count()
       .sort($"count".desc)
 
     println("Capturing Twitter Stream...")
-    query.writeStream.outputMode("complete").format("console").start().awaitTermination(60000)
+    badWordCount.writeStream.outputMode("complete").format("console").start().awaitTermination(60000)
 
-    query.printSchema()
-    dictionary.destroy()
+    badWordCount.printSchema()
     spark.close()
   }
 }
