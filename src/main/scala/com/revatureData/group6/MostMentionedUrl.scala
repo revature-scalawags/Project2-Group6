e1d81a01446fb434bcae517aa960f6b10b018cd5
David Masterson <davidmasterson@Davids-MacBook-Pro.local>
2021-01-21 07:14:29 -0600

    finished MostMentionedURL jar

--- src/main/scala/com/revatureData/group6/MostMentionedUrl.scala
+++ src/main/scala/com/revatureData/group6/MostMentionedUrl.scala
@@ -2,39 +2,44 @@ package com.revatureData.group6
 
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
+import scala.util.matching.Regex
+import org.apache.spark.rdd.RDD
+import scala.collection.mutable.ListBuffer
 
 
 
 object MostNotedUrl {
     
-    case class Tweet(value: String)
+    case class Tweet(data: Data)
+    case class Data(id: String,text: String)
     
 
-    def main(args: Array[String]){
+    def main(args: Array[String]): Unit={
         
-        //i need to be able to get a list of file names at
+        
         
 
         val spark = SparkSession
             .builder
-            .appName(name="MostNotedLink")
+            .appName(name="MostNotedUrl")
+            .master(master="local[*]")
             .getOrCreate()
 
         import spark.implicits._
 
-        val input = spark.read.text(path = "s3://group6project2/Datalake/twitterstream/combined.txt").as[Tweet]
+        val input = spark.read.json(path = "s3://group6project2/Datalake/twitterstream/combined.txt").as[Tweet]
 
         val urls = input
-            .select(explode(split(str=$"value", pattern="http*://*{1,}\\s")).alias(alias="url"))
+            .select(explode(split(str=$"data.text", pattern="\\s")).alias(alias="url"))
             .filter(condition=$"url" =!= "")
-
-        val lowercaseUrls = urls.select(lower($"url").alias(alias="url"))
-
-        val urlCounts = lowercaseUrls.groupBy(col1="url").count()
-
-        val urlCountsSorted = urlCounts.sort(sortCol = "count")
-
-        urlCountsSorted.show(urlCountsSorted.count.toInt)
+        val urlDF = urls.toDF()
+        
+        
+        val links = urlDF.filter($"url" rlike "(http)" )
+        val linkCounts = links.groupBy(col1="url").count()
+        val linkCountsSorted = linkCounts.sort("count")
+        linkCountsSorted.show(linkCountsSorted.count.toInt)
+        
 
     }   
 }
